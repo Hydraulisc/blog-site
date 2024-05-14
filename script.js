c4cd2173ce5db7636ba655633ef4077fe1d341f6
SleepingAmi <143962821+SleepingAmi@users.noreply.github.com>
2024-05-14 11:31:09 +0200

    Add the actual blog display(s)

---
+++ script.js
@@ -0,0 +1,93 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDV4JvWqDdFWJjcH9DGopZxeIeHTtsDTEA",
+    authDomain: "hydraul-blog.firebaseapp.com",
+    projectId: "hydraul-blog",
+    storageBucket: "hydraul-blog.appspot.com",
+    messagingSenderId: "18958256761",
+    appId: "1:18958256761:web:6d0081cb0a671e62f562a8",
+    measurementId: "G-9N109WZ5NJ"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+const store = firebase.firestore();
+const analytics = firebase.analytics();
+
+const loadingPosts = 5;
+let lastVisiblePost = null;
+let morePostsAvailable = true;
+
+window.onload = function() {
+    const entryLocator = new URLSearchParams(window.location.search.slice(1));
+    const entryID = entryLocator.get('entry');
+    const titleContainer = document.getElementById("title");
+    const buttonContainer = document.getElementById("buttons");
+    const postContainer = document.getElementById("bodycontainer");
+    if(!entryID){
+        titleContainer.innerHTML = `<h1 style="color: #5667c7 !important;">The <span style="text-decoration: underline !important;">Hydraulisc</span> Blog</h1>`;
+        buttonContainer.innerHTML = `<button id="loadMore" class="btn-primary primary-text">Load More</button>`;
+        getPosts();
+    } else {
+        store.collection("entries").where("path", "==", entryID)
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    titleContainer.innerHTML = `<h1>${doc.data().title}</h1>`;  
+                    buttonContainer.innerHTML = `<button onclick="location.href='/';" class="btn-primary primary-text">Return Home</button>`;
+                    postContainer.innerHTML = `<p style="font-size: 10px; position: absolute; top: 52px; left: 32px;" title="${doc.data().update}">${doc.data().update}</p>${doc.data().content}`;
+            });
+        })
+    }
+}
+
+
+function getPosts() {
+    const postsRef = firebase.firestore().collection('entries');
+    let query = postsRef.orderBy('pnum', 'desc').limit(loadingPosts);
+
+    if (lastVisiblePost) {
+        query = query.startAfter(lastVisiblePost);
+    }
+
+    query.onSnapshot(function(snapshot) {
+        // Check if there are more posts available to load
+        if (snapshot.size < loadingPosts) {
+            morePostsAvailable = false;
+        }
+        var posts = snapshot.docs.map(function(post) {
+            return post.data();
+        });
+
+        posts.forEach(function(post) {
+            addPostToPage(post);
+        });
+
+        lastVisiblePost = snapshot.docs[snapshot.docs.length - 1];
+    });
+}
+
+function addPostToPage(post) {
+    var postElement = `
+    <br>
+    <div style="border: 2px solid;" class="carrd ${post.path}">
+        <div style="position: relative;">
+            <h3 style="cursor: default; position: relative; left: 10px;">${post.title}</h3>
+            <p style="font-size: 10px; position: absolute; top: -10px; right: 29px;">${post.update}</p>
+        </div>
+        <button style="width: 100%; border: none; border-radius: 10px 10px 0px 0px; background-color: #5667c7; text-transform: uppercase; cursor: pointer;" class="primary-text" onclick="location.href='/?entry=${post.path}';">read</button>
+    </div>
+  `;
+  document.getElementById("entries").innerHTML += postElement;
+};
+
+document.addEventListener('click', (e) => {
+    let element = e.target;
+    if(morePostsAvailable){
+        if(element.id == 'loadMore'){
+            getPosts();
+        }
+    } else {
+        return;   
+    }
+
+})
